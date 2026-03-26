215bb045020167fef3ea4bd61aa58c1aee9d0a58
G-Gamboa <gamboaguillermo12@gmail.com>
2026-03-26 14:48:17 -0600

    CORS

--- app/main.py
+++ app/main.py
@@ -21,10 +21,10 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.cors_origins,
+        allow_origins=settings.CORS_ORIGINS,
         allow_credentials=True,
-        allow_methods=['*'],
-        allow_headers=['*'],
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     app.include_router(health_router)
