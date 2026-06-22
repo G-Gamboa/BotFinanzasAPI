e01734f926db449a05a0ae97a3ce5aa0b93ac8ac
G-Gamboa <gamboaguillermo12@gmail.com>
2026-06-22 12:39:12 -0600

    Modify CORS

--- app/main.py
+++ app/main.py
@@ -97,7 +97,7 @@ app.add_middleware(SlowAPIMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type"],
 )
